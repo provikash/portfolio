3f0e75297b7ed9853532a77a7d8bca7709605294
Vikash <vkchaurasiyatrade@gmail.com>
2026-07-19 03:59:18 +0530

    updated build

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-git clone https://github.com/flutter/flutter.git --depth 1
+git clone https://github.com/flutter/flutter.git --depth 1 -b 3.44.4
 export PATH="$PWD/flutter/bin:$PATH"
 
 flutter config --enable-web
